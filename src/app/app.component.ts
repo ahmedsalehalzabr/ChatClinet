5ce49fbbd0405b72aad524f51316d33e86e502e4
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-02-16 07:01:21 -1200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clinet';
+  users: any;
+constructor(private http: HttpClient){
+
+}
+ngOnInit(): void {
+  this.http.get('https://localhost:7126/api/Users').subscribe({
+    next: response => this.users = response,
+    error: error => console.log(error),
+    complete: () => console.log('completed')
+  })
+}
+
 }
